240cd85217b8240e2b1b4f15c708e9caa8dfd93b
twothicc <chenenliuu@gmail.com>
2020-02-17 21:50:07 +0800

    Added Project to repository

---
+++ Assets/Scripts/HangmanDisplayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HangmanDisplayer : MonoBehaviour
+{
+    private static int Hangman = 0;
+    public GameObject[] HangmanParts = new GameObject[10];
+    public int GetHangman
+    {
+        get { return Hangman; }
+        set { Hangman = value; }
+    }
+    void Update()
+    {
+        for (int i = 0; i < Hangman; i++)
+        {
+            HangmanParts[i].GetComponent<MeshRenderer>().enabled = true;
+        }
+        if (Hangman == 10)
+        {
+            SceneManager.LoadScene("lose");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    public AudioSource MP3;
+    public void Playmusic()
+    {
+        MP3.Play();
+    }
+}
